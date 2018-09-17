d66750be7914e66930007200f96e649a8a6725b6
Miloslav Číž <tastyfish@seznam.cz>
2018-09-17 12:06:14 +0200

    Fix floor depth

--- raycastlib.h
+++ raycastlib.h
@@ -1220,7 +1220,7 @@ Unit coordStep = 1;
   // draw floor
 
   p.isWall = 0;
-  p.depth = _middleRow * _horizontalDepthStep;
+  p.depth = (_middleRow - y) * _horizontalDepthStep;
 
   while (y < _camera.resolution.y)
   {
